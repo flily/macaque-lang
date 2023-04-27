b41033d8b5848cc9d42aada1e0c3c6f268715e18
Flily Hsu <1126143+flily@users.noreply.github.com>
2023-04-27 16:07:45 +0800

    fix float24 and add test cases

--- types/float/float24.go
+++ types/float/float24.go
@@ -1,7 +1,10 @@
 // Special encoding for 24-bit floating point numbers, used in 32-bit instruction
 package float
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // IEEE-754 float number
 //  Size   Sign   Exponent   Fraction
@@ -12,6 +15,24 @@ import "fmt"
 //  128      1        15         112
 
 type Float24 uint32
+
+var (
+	PositiveZero64     = float64(0.0)
+	NegativeZero64     = buildFloat64(1, -1023, 0)
+	PositiveInfinity64 = float64(math.Inf(1))
+	NegativeInfinity64 = float64(math.Inf(-1))
+	NaN24              = buildFloat24(0, 32, 1)
+
+	PositiveZero32     = float32(0.0)
+	NegativeZero32     = buildFloat32(1, -127, 0)
+	PositiveInfinity32 = float32(math.Inf(1))
+	NegativeInfinity32 = float32(math.Inf(-1))
+
+	PositiveZero24     = buildFloat24(0, -31, 0)
+	NegativeZero24     = buildFloat24(1, -31, 0)
+	PositiveInfinity24 = buildFloat24(0, 32, 0)
+	NegativeInfinity24 = buildFloat24(1, 32, 0)
+)
 
 func buildFloat24(sign uint32, exp int64, frac uint64) Float24 {
 	u := (sign&1)<<23 |
@@ -29,20 +50,26 @@ func splitFloat24(f Float24) (uint32, int64, uint64) {
 	return sign, exp, frac
 }
 
-func NewPositiveZero() Float24 {
-	return buildFloat24(0, -31, 0)
-}
+func NewFloat24(f float64) Float24 {
+	sign, exp, frac := splitFloat64(f)
+	switch {
+	case exp >= 1024 && frac != 0: // NaN
+		if frac&0x1ffff == 0 {
+			frac = 1
+		}
+		exp = 32
 
-func NewNegativeZero() Float24 {
-	return buildFloat24(1, -31, 0)
-}
+	case exp >= 32: // Infinity
+		exp, frac = 32, 0
 
-func NewPositiveInfinite() Float24 {
-	return buildFloat24(0, 32, 0)
-}
+	case exp <= -31: // Zero
+		exp, frac = -31, 0
 
-func NewNegativeInfinite() Float24 {
-	return buildFloat24(1, 32, 0)
+	default:
+		frac = frac >> 35
+	}
+
+	return buildFloat24(sign, exp, frac)
 }
 
 func (f Float24) String() string {
@@ -57,6 +84,8 @@ func (f Float24) String() string {
 		s = "+Inf"
 	case sign == 1 && exp == 32 && frac == 0:
 		s = "-Inf"
+	case exp == 32 && frac != 0:
+		s = "NaN"
 	default:
 		s = fmt.Sprintf("%f", buildFloat32(sign, exp, frac<<6))
 	}
@@ -64,7 +93,34 @@ func (f Float24) String() string {
 	return s
 }
 
-func NewFloat24FromFloat32(f float32) Float24 {
-	sign, exp, frac := splitFloat32(f)
-	return buildFloat24(sign, exp, frac>>6)
+func (f Float24) Float32() float32 {
+	sign, exp, frac := splitFloat24(f)
+	if exp <= -31 {
+		exp = -127
+	} else if exp >= 32 {
+		exp = 128
+	}
+
+	return buildFloat32(sign, exp, frac<<6)
+}
+
+func (f Float24) Float64() float64 {
+	sign, exp, frac := splitFloat24(f)
+	if exp <= -31 {
+		exp = -1023
+	} else if exp >= 32 {
+		exp = 1024
+	}
+
+	return buildFloat64(sign, exp, frac<<35)
+}
+
+func (f Float24) IsNaN() bool {
+	_, exp, frac := splitFloat24(f)
+	return exp == 32 && frac != 0
+}
+
+func (f Float24) IsInf() bool {
+	_, exp, frac := splitFloat24(f)
+	return exp == 32 && frac == 0
 }
---
+++ types/float/float24_test.go
@@ -0,0 +1,177 @@
+package float
+
+import (
+	"math"
+	"testing"
+)
+
+func TestFloat24Zero(t *testing.T) {
+	pz := PositiveZero24
+	pz32 := PositiveZero32
+	if pz.Float32() != pz32 {
+		t.Errorf("+0.0.Float32() = %v, want %f", pz, pz32)
+	}
+
+	pz64 := float64(0.0)
+	if pz.Float64() != pz64 {
+		t.Errorf("+0.0.Float32() = %v, want %f", pz, pz64)
+	}
+
+	if pz.String() != "+0.000" {
+		t.Errorf("+0.0.String() = %v, want %v", pz, "+0.000")
+	}
+
+	nz := NegativeZero24
+	nz32 := NegativeZero32
+	if nz.Float32() != nz32 {
+		t.Errorf("-0.0.Float32() = %v, want %f", nz, nz32)
+	}
+
+	nz64 := NegativeZero64
+	if nz.Float64() != nz64 {
+		t.Errorf("-0.0.Float32() = %v, want %f", nz, nz64)
+	}
+
+	if nz.String() != "-0.000" {
+		t.Errorf("-0.0.String() = %v, want %v", nz, "-0.000")
+	}
+}
+
+func TestFloat24Infinity(t *testing.T) {
+	pi := PositiveInfinity24
+
+	pi32 := PositiveInfinity32
+	if pi.Float32() != pi32 {
+		t.Errorf("+Inf.Float32() = %v, want %f", pi, pi32)
+	}
+
+	pi64 := PositiveInfinity64
+	if pi.Float64() != pi64 {
+		t.Errorf("+Inf.Float64() = %v, want %f", pi, pi64)
+	}
+
+	if pi.String() != "+Inf" {
+		t.Errorf("+Inf.String() = %v, want %v", pi, "+Inf")
+	}
+
+	if !pi.IsInf() {
+		t.Errorf("+Inf.IsInf() = %v, want %v", pi, true)
+	}
+
+	ni := NegativeInfinity24
+	ni32 := NegativeInfinity32
+	if ni.Float32() != ni32 {
+		t.Errorf("-Inf.Float32() = %v, want %f", ni, ni32)
+	}
+
+	ni64 := NegativeInfinity64
+	if ni.Float64() != ni64 {
+		t.Errorf("-Inf.Float64() = %v, want %f", ni, ni64)
+	}
+
+	if ni.String() != "-Inf" {
+		t.Errorf("-Inf.String() = %v, want %v", ni, "-Inf")
+	}
+
+	if !ni.IsInf() {
+		t.Errorf("-Inf.IsInf() = %v, want %v", ni, true)
+	}
+}
+
+func TestFloat24NaN(t *testing.T) {
+	nan64s := []float64{
+		math.NaN(),
+		buildFloat64(0, 1024, 0xffffffff),
+	}
+
+	for _, f := range nan64s {
+		if !math.IsNaN(f) {
+			t.Errorf("math.IsNaN(%v) = %v, want %v", f, math.IsNaN(f), true)
+		}
+	}
+
+	nan24s := []Float24{
+		NaN24,
+		NewFloat24(math.NaN()),
+		NewFloat24(buildFloat64(0, 1024, 0xffffffff)),
+		NewFloat24(buildFloat64(0, 1024, 0xfff00000)),
+	}
+
+	for _, nan := range nan24s {
+		if !nan.IsNaN() {
+			t.Errorf("%s.IsNaN() = %v, want %v", nan, nan.IsInf(), true)
+		}
+
+		if nan.String() != "NaN" {
+			t.Errorf("NaN.String() = %v, want %v", nan, "NaN")
+		}
+	}
+}
+
+func TestNewInfinity(t *testing.T) {
+	bigNumber := float64(1e100)
+	if math.IsInf(bigNumber, 1) {
+		t.Errorf("math.IsInf(%v, 1) = %v, want %v", bigNumber, math.IsInf(bigNumber, 1), true)
+	}
+
+	if math.IsNaN(bigNumber) {
+		t.Errorf("math.IsNaN(%v) = %v, want %v", bigNumber, math.IsNaN(bigNumber), false)
+	}
+
+	f24 := NewFloat24(bigNumber)
+	if !f24.IsInf() {
+		t.Errorf("NewFloat24(%v).IsInf() = %v, want %v", bigNumber, f24.IsInf(), true)
+	}
+}
+
+func TestNewZero(t *testing.T) {
+	smallNumber := float64(1e-100)
+
+	if smallNumber < 0 {
+		t.Errorf("%v < 0", smallNumber)
+	}
+
+	if !(smallNumber > 0) {
+		t.Errorf("%v > 0", smallNumber)
+	}
+
+	f24 := NewFloat24(smallNumber)
+	f64 := f24.Float64()
+	if f64 < 0 {
+		t.Errorf("%v < 0", f64)
+	}
+
+	if f64 > 0 {
+		t.Errorf("%v > 0", f64)
+	}
+}
+
+func TestFloat24String(t *testing.T) {
+	tests := []struct {
+		number   float64
+		expected string
+	}{
+		{math.NaN(), "NaN"},
+		{math.Inf(1), "+Inf"},
+		{math.Inf(-1), "-Inf"},
+		{0.0, "+0.000"},
+		{NegativeZero64, "-0.000"},
+		{1.0, "1.000000"},
+		{99.5, "99.500000"},
+
+		// Cases which lose precision.
+		{3.3, "3.299988"},
+		{99.2, "99.199707"},
+		{9.80665, "9.806641"},
+		{3.1415926, "3.141586"},
+		{299792458, "299792384.000000"},
+		{9.80665, "9.806641"},
+	}
+
+	for _, test := range tests {
+		f24 := NewFloat24(test.number)
+		if f24.String() != test.expected {
+			t.Errorf("%v.String() = %v, want %v", test.number, f24.String(), test.expected)
+		}
+	}
+}
